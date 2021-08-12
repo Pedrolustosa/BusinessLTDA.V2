b1ef4ec8365ba40d21c3c96d3bfd997c9ba2a272
Pedro Lustosa <pedroeternalss@gmail.com>
2021-08-12 20:22:32 -0300

    Methods Gets, Posts, Puts, Deletes and photos.

--- BusinessLTDA-APP/src/app/department/department.component.ts
+++ BusinessLTDA-APP/src/app/department/department.component.ts
@@ -12,6 +12,10 @@ export class DepartmentComponent implements OnInit {
   constructor(private http: HttpClient) { }
   departments: any = []
 
+  modalTitle = "";
+  DepartmentId = 0;
+  DepartmentName = "";
+
   ngOnInit(): void {
     this.refreshList();
   }
@@ -21,6 +25,50 @@ export class DepartmentComponent implements OnInit {
       .subscribe(data => {
         this.departments = data;
       })
+  }
+
+  addClick(): void{
+    this.modalTitle="Novo Departamento";
+    this.DepartmentId = 0;
+    this.DepartmentName = "";
+  }
+
+  editClick(dep: any){
+    this.modalTitle="Editar Departamento";
+    this.DepartmentId = dep.DepartmentId;
+    this.DepartmentName = dep.DepartmentName;
+  }
+
+  createClick(){
+    var val = {
+      DepartmentName: this.DepartmentName
+    };
+    this.http.post(environment.API_URL+'departments', val)
+    .subscribe(res => {
+      alert(res.toString());
+      this.refreshList();
+    });
+  }
+
+  updateClick(){
+    var val = {
+      DepartmentId: this.DepartmentId,
+      DepartmentName: this.DepartmentName
+    };
+    this.http.put(environment.API_URL+'departments', val)
+    .subscribe(res => {
+      alert(res.toString());
+      this.refreshList();
+    });
+  }
+
+  deleteClick(id: any){
+    if(confirm('Você deseja mesmo excluir ?'))
+    this.http.delete(environment.API_URL+'departments/'+id)
+    .subscribe(res => {
+      alert(res.toString());
+      this.refreshList();
+    });
   }
 
 }
